d1110ca17d8d419d87abf0402dce4a8c1ef42c01
ceoRosez <jamalmoore729@gmail.com>
2025-02-26 02:38:51 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+music.play(music.stringPlayable("A - F D - C5 C - ", 135), music.PlaybackMode.InBackground)
+game.onUpdate(function () {
+    left toogle moves foward and rright toogle moves player backwards
+})a button rapledy hits enemys, b button jumps
+game.onUpdateInterval(scene.screenWidth(), function () {
+    up toogle moves chacter left and down toggle moves chacter right
+})over 35 diffent levels and 100 different enemies
